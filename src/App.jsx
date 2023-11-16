1cd25e430bb65ae49c2c8ea0f6861bfe4fe1dae2
jamilharun <jamilharunl45@gmail.com>
2023-11-16 15:07:40 +0800

    tailwind css is not working

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs via-fuchsia-600">
+      <p className="read-the-docs via-fuchsia-600 text-xl font-black">
         Click on the Vite and React logos to learn more
       </p>
     </>
