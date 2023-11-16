fd47fa868be07592721d196fbd27aad7a4b0f009
jamilharun <jamilharunl45@gmail.com>
2023-11-16 16:53:51 +0800

    dotenv undefined

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,8 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+          {process.env.SECRET_KEY}
+          {process.env.API_KEY}
         </p>
       </div>
       <p className="read-the-docs text-violet-900">
