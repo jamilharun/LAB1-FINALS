18b9361cb175cee34326988b0bd1f15548af997f
jamilharun <jamilharunl45@gmail.com>
2023-11-16 16:26:41 +0800

    tailwindcss fix

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs via-fuchsia-600 text-xl font-black">
+      <p className="read-the-docs text-violet-900">
         Click on the Vite and React logos to learn more
       </p>
     </>
